04f5cf39a007ff085693b3f3fa8b27efcaa3f338
James <jfnelson2000@gmail.com>
2023-04-29 14:24:31 -0700

    use state implemented with filler data

--- client/src/Components/Tasklist.jsx
+++ client/src/Components/Tasklist.jsx
@@ -1,11 +1,20 @@
-import React from "react"
+import React, {useState} from "react"
 import ToDo from "./ToDo"
 
 function Tasklist () {
+    const [todo, setTodo] = useState([
+        { text: 'task one', id: 1 },
+        { text: 'task two', id: 2 },
+    ])
+
     return (
         <div>
             <h1>Task List</h1>
-            <ToDo />
+            {todo.map((task) => (
+                <div className="todo" key={task.id}>
+                    <p>{task.text}</p>
+                </div>
+            ))}
         </div>
     )
 }
