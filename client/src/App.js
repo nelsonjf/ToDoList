42973ed8bcf3e19c9707e4bf5d8b756eaea0edb1
James <jfnelson2000@gmail.com>
2023-04-29 12:28:10 -0700

    new component

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Navbar from "./Components/Navbar";
 import Tasklist from "./Components/Tasklist";
 
---
+++ client/src/Components/New.jsx
@@ -0,0 +1,9 @@
+function New () {
+    return (
+        <div>
+            <h1>New ToDo</h1>
+        </div>
+    )
+}
+
+export default New
