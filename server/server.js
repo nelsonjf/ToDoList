04eed9b6dabc5094c0973896a00b54f7dfc0780a
James <jfnelson2000@gmail.com>
2023-04-30 10:38:54 -0700

    server routes established

--- server/server.js
+++ server/server.js
@@ -4,4 +4,33 @@ const app = express()
 
 app.use(express.json())
 
+mongoose.connect("mongodb://localhost/todolist")
+
+const Todo = require('./models/todo')
+
+app.get('/', async (req, res) => {
+    const todos = await Todo.find()
+    res.json(todos)
+})
+
+app.post('/new', (req, res) => {
+    const todo = new Todo({
+        task: req.body.text
+    })
+    todo.save()
+    res.json(todo)
+})
+
+app.delete('/delete/:id', async(req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id)
+    res.json(result)
+})
+
+app.put('/complete/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id)
+    todo.done = !todo.done
+    todo.save()
+    res.json(todo)
+})
+
 app.listen(4000, () => console.log("Server running on port 4000"))
