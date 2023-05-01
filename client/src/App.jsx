6c33eac3b2f53e193ea64d3434f7c86c8da34630
James <jfnelson2000@gmail.com>
2023-05-01 14:16:21 -0700

    front end connected to local db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,51 @@
 import React, { useState, useEffect } from "react";
 
+const API_BASE = "http://localhost:4000"
+
 function App() {
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodos] = useState('')
+
+  useEffect(() => {
+    GetTodos()
+    console.log(todos)
+  }, [])
+
+  const GetTodos = () => {
+      fetch(API_BASE + "/todo")
+        .then(res => res.json())
+        .then(data => setTodos(data))
+        .catch(err => console.error("Error: ", err))
+  }
+
   return (
     <div className="App">
       <h1>Welcome, User</h1>
+
       <div className="new-todo">
         <form>
           <input type="text" placeholder="Enter new Todo here" />
           <input type="submit" value="Submit" />
         </form>
       </div>
+
       <div className="todo-list">
         <h2>To Do List:</h2>
-        <div className="todo">
-          <p>Task</p>
-        </div>
-        <div className="todo">
-          <p>Task</p>
-        </div>
-        <div className="todo">
-          <p>Task</p>
-        </div>
+        {todos.map(todo => (
+          <div className="todo" key={todo.id}>
+            <p>{todo.todo}</p>
+          </div>
+        ))}
       </div>
+
     </div>
   );
 }
 
 export default App;
+
+////////////////////////////////////////////////
+// Potts, T (2021)                            //
+// mern-todo [source code]                    //
+// https://github.com/TylerPottsDev/mern-todo //
+////////////////////////////////////////////////
--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+}).then(() => console.log("Connected to Database"))
 
 const Todo = require('./models/todo')
 const todo = require('./models/todo')
