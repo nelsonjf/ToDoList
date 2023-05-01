82e998648972c679d80476d4ff96a004bcf00275
James <jfnelson2000@gmail.com>
2023-04-30 19:46:41 -0700

    post request for new todo functioning

--- server/server.js
+++ server/server.js
@@ -12,4 +12,27 @@ mongoose.connect(process.env.MONGO_URI, {
 
 const Todo = require('./models/todo')
 
+function test() {
+    let newtodo = new Todo({
+        todo: "test"
+    })
+    newtodo.save()
+}
+
+//test()
+
+app.get('/', async (req, res) => {
+    const todo = await Todo.find()
+
+    res.json(todo)
+})
+
+app.post('/new', (req, res) => {
+    const todo = new Todo({
+        todo: req.body.todo
+    })
+    todo.save()
+    res.json(todo)
+})
+
 app.listen(process.env.PORT, () => console.log("Server running"))
