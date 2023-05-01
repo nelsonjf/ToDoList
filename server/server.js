9811f23539e620729a417406ca5f588d0c9db230
James <jfnelson2000@gmail.com>
2023-04-30 18:42:59 -0700

    removed nonfunctional server paths

--- server/server.js
+++ server/server.js
@@ -9,29 +9,4 @@ mongoose.connect("mongodb://localhost/todolist")
 
 const Todo = require('./models/todo')
 
-app.get('/todo', async (req, res) => {
-    const todos = await Todo.find()
-    res.json(todos)
-})
-
-app.post('/todo/new', (req, res) => {
-    const todo = new Todo({
-        task: req.body.text
-    })
-    todo.save()
-    res.json(todo)
-})
-
-app.delete('/todo/delete/:id', async(req, res) => {
-    const result = await Todo.findByIdAndDelete(req.params.id)
-    res.json(result)
-})
-
-app.put('/todo/complete/:id', async (req, res) => {
-    const todo = await Todo.findById(req.params.id)
-    todo.done = !todo.done
-    todo.save()
-    res.json(todo)
-})
-
 app.listen(process.env.PORT, () => console.log("Server running"))
