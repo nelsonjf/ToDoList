3e06fb15712f425a225b128427926a98db389f5d
James <jfnelson2000@gmail.com>
2023-05-01 13:56:58 -0700

    removed components entirely

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Tasklist from "./Components/Tasklist";
-
 
 function App() {
   return (
     <div className="App">
-      <Tasklist />
+
     </div>
   );
 }
