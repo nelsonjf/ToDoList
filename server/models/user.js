38233bac46087aac1f53d15a4c6a832eba06dcbf
James <jfnelson2000@gmail.com>
2023-05-02 23:45:56 -0700

    email and password validation plus new server error

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -15,10 +16,22 @@ const userSchema = new mongoose.Schema({
 
 // Signup method
 userSchema.statics.signup = async function (email, password) {
+    // validation
+    if (!email || !password) {
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)) {
+        throw Error('Email is not valid')
+    }
+    if (!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+    // check for existing email
     const exists = await this.findOne({ email })
     if (exists) {
         throw Error('Email already in use')
     }
+    // password encryption
     const salt = await bcrypt.genSalt(10)
     const hash = await bcrypt.hash(password, salt)
     const user = await this.create({email, password: hash})
